a078abc0c95e44aabc1eed86006c3b4e4ae81d27
Haibao Tang <tanghaibao@gmail.com>
2017-11-06 16:27:11 -0700

    [hic] Implement hic.clm.parse_ids()

---
+++ hic/clm.nim
@@ -0,0 +1,52 @@
+from parseutils import parseInt
+from strutils import rsplit, split
+
+
+#[
+CLM file (modified) has the following format:
+    tig00046211+ tig00063795+       1       53173
+    tig00046211+ tig00063795-       1       116050
+    tig00046211- tig00063795+       1       71155
+    tig00046211- tig00063795-       1       134032
+    tig00030676+ tig00077819+       7       136407 87625 87625 106905 102218 169660 169660
+    tig00030676+ tig00077819-       7       126178 152952 152952 35680 118923 98367 98367
+    tig00030676- tig00077819+       7       118651 91877 91877 209149 125906 146462 146462
+    tig00030676- tig00077819-       7       108422 157204 157204 137924 142611 75169 75169
+]#
+
+type
+  CLMFile* = ref object
+    name*: string
+    clmfile*: string
+    idsfile*: string
+  Tig* = tuple[tig: string, size: int]
+
+
+proc newCLMFile*(name: string, clmfile: string): CLMFile =
+  new result
+  result.name = name
+  result.clmfile = clmfile
+  result.idsfile = clmfile.rsplit('.', maxsplit=1)[0] & ".ids"
+
+
+#[
+IDS file has a list of contigs that need to be ordered. 'recover',
+keyword, if available in the third column, is less confident.
+        tig00015093     46912
+        tig00035238     46779   recover
+        tig00030900     119291
+#]#
+proc parse_ids*(this: CLMFile, skiprecover: bool): seq[Tig] =
+  result = @[]
+  for line in lines this.idsfile:
+    let atoms = line.split()
+    let tig = atoms[0]
+    var size = 0
+    discard parseInt(atoms[1], size)
+    result.add((tig: tig, size: size))
+  return result
+
+
+proc parse*(this: CLMFile) =
+  for line in lines this.clmfile:
+    echo line
--- hic_opt.nim
+++ hic_opt.nim
@@ -1,1 +1,5 @@
-echo "Hello world"
+from hic/clm import newCLMFile, parse, parse_ids
+
+var c = newCLMFile("test", "tests/test.clm")
+echo c.parse_ids(true)
+#c.parse()
