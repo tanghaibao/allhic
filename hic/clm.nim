b040c3eb2b14d8f61110732cd4afd1a4b902c0f0
Haibao Tang <tanghaibao@gmail.com>
2017-11-06 18:00:15 -0700

    [hic] Add hic.clm.parse_clm()

--- hic/clm.nim
+++ hic/clm.nim
@@ -1,19 +1,20 @@
+import future
 import parseutils
 import strutils
 import tables
 
 
-#[
-CLM file (modified) has the following format:
-    tig00046211+ tig00063795+       1       53173
-    tig00046211+ tig00063795-       1       116050
-    tig00046211- tig00063795+       1       71155
-    tig00046211- tig00063795-       1       134032
-    tig00030676+ tig00077819+       7       136407 87625 87625 106905 102218 169660 169660
-    tig00030676+ tig00077819-       7       126178 152952 152952 35680 118923 98367 98367
-    tig00030676- tig00077819+       7       118651 91877 91877 209149 125906 146462 146462
-    tig00030676- tig00077819-       7       108422 157204 157204 137924 142611 75169 75169
-]#
+##
+## CLM file (modified) has the following format:
+##    tig00046211+ tig00063795+       1       53173
+##    tig00046211+ tig00063795-       1       116050
+##    tig00046211- tig00063795+       1       71155
+##    tig00046211- tig00063795-       1       134032
+##    tig00030676+ tig00077819+       7       136407 87625 87625 106905 102218 169660 169660
+##    tig00030676+ tig00077819-       7       126178 152952 152952 35680 118923 98367 98367
+##    tig00030676- tig00077819+       7       118651 91877 91877 209149 125906 146462 146462
+##    tig00030676- tig00077819-       7       108422 157204 157204 137924 142611 75169 75169
+##
 
 type
   CLMFile* = ref object
@@ -31,26 +32,25 @@ proc initCLMFile*(name: string, clmfile: string): CLMFile =
   result.tig_to_size = initOrderedTable[string, int]()
 
 
-#[
-IDS file has a list of contigs that need to be ordered. 'recover',
-keyword, if available in the third column, is less confident.
-        tig00015093     46912
-        tig00035238     46779   recover
-        tig00030900     119291
-#]#
+##
+## IDS file has a list of contigs that need to be ordered. 'recover',
+## keyword, if available in the third column, is less confident.
+##        tig00015093     46912
+##        tig00035238     46779   recover
+##        tig00030900     119291
+##
 proc parse_ids*(this: CLMFile, skiprecover: bool) =
-  var
-    tig: string
-    size: int
-
   for line in lines this.idsfile:
     let atoms = line.split()
-    tig = atoms[0]
-    size = 0
-    discard parseInt(atoms[1], size)
+    let tig = atoms[0]
+    let size = parseInt(atoms[1])
     this.tig_to_size[tig] = size
 
 
-proc parse*(this: CLMFile) =
+proc parse_clm*(this: CLMFile) =
   for line in lines this.clmfile:
-    echo line
+    let atoms = line.strip().split('\t')
+    let abtig = atoms[0]
+    let links = parseInt(atoms[1])
+    let dists = lc[parseInt(x) | (x <- atoms[2].split()), int]
+    echo dists
--- hic_opt.nim
+++ hic_opt.nim
@@ -5,9 +5,7 @@ import tables
 proc main() {.discardable.} =
   var c = initCLMFile("test", "tests/test.clm")
   c.parse_ids(true)
-  for key, val in c.tig_to_size.pairs():
-    echo("$# $#".format(key, val))
-  #c.parse()
+  c.parse_clm()
 
 
 when isMainModule:
