4a03efcdbee55fd65afc02e5d8146bfd25d367f4
Haibao Tang <tanghaibao@gmail.com>
2017-11-20 23:51:20 -0700

    [allhic] Add docopt

--- allhic.nim
+++ allhic.nim
@@ -1,10 +1,38 @@
+#
+# ALLHIC: genome scaffolding based on Hi-C data
+# Author: Haibao Tang <tanghaibao at gmail dot com>
+#
+
+import docopt
 import hic/clm
 import strutils
 import tables
 
-proc main() {.discardable.} =
+let doc = """
+ALLHIC: genome scaffolding based on Hi-C data
+
+Usage:
+  allhic partition
+  allhic optimize
+
+Options:
+  -h --help       Show this screen.
+  --version       Show version.
+"""
+
+proc optimize_main() =
   var c = initCLMFile("test", "tests/test.clm")
   c.parse()
+
+
+proc main() =
+  let args = docopt(doc, version="ALLHIC 0.7.11")
+
+  if args["partition"]:
+    echo "Hello world"
+
+  if args["optimize"]:
+    optimize_main()
 
 
 when isMainModule:
