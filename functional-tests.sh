5703c1ab788b15e784d82fb694fd698048623dae
Haibao Tang <htang@humanlongevity.com>
2018-01-06 09:52:25 -0800

    [CI] Add .travis.yml and functional tests

---
+++ functional-tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+
+. ssshtest
+set -uo pipefail
+
+go build -o main_test main.go
+
+./main_test optimize
