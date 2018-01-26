ddb59680746be0559ecc0571996f55dba36f78f3
Haibao Tang <htang@humanlongevity.com>
2018-01-25 20:37:11 -0800

    [partition] Add eigen decomposition in go

--- script/newman_modularity.go
+++ script/newman_modularity.go
@@ -20,6 +20,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/gonum/matrix/mat64"
 	logging "github.com/op/go-logging"
 )
 
@@ -40,6 +41,14 @@ type Edge struct {
 	weight int
 }
 
+// Graph represents the graph
+type Graph struct {
+	A        [][]int
+	nodes    []string
+	nodesIdx map[string]int
+	m, n     int
+}
+
 // Make2DSlice allocates a 2D matrix with shape (m, n)
 func Make2DSlice(m, n int) [][]int {
 	P := make([][]int, m)
@@ -51,7 +60,7 @@ func Make2DSlice(m, n int) [][]int {
 
 // ParseGraph imports a graph in its "edge list" form. Returns an adjacency matrix.
 // a b weight
-func ParseGraph(filename string) [][]int {
+func ParseGraph(filename string) Graph {
 	file, _ := os.Open(filename)
 	reader := bufio.NewReader(file)
 	var edges []Edge
@@ -92,18 +101,74 @@ func ParseGraph(filename string) [][]int {
 
 	m := len(edges)
 	n := len(nodes)
-	log.Noticef("Graph contains %d nodes and %d edges", m, n)
+	log.Noticef("Graph contains %d nodes and %d edges", n, m)
 
 	A := Make2DSlice(n, n)
 	for _, e := range edges {
 		A[e.a][e.b] = e.weight
 		A[e.b][e.a] = e.weight
 	}
-	return A
+
+	g := Graph{A, nodes, nodesIdx, m, n}
+
+	return g
+}
+
+// NewmanPartition partitions the graph to maximize 'modularity'
+// Newman modularity inference method:
+//
+//     Q = 1/4m Sum_ij(A_ij - k_i * k_j / 2m) s_i * s_j
+//
+//     m: total number of edges
+//     A_ij: adjancency matrix
+//     k_i, k_j: degree of node i, j
+//     s_i, s_j: partition of node i, j, either +1 or -1
+//
+//     We can conveniently write Q in matrix form
+//
+//     Q = 1/4m s.T * B * s
+//
+//     Where B_ij = A_ij - k_i * k_j / 2m
+func NewmanPartition(g Graph) {
+	var (
+		M mat64.Dense
+		e mat64.EigenSym
+	)
+	B := mat64.NewSymDense(g.n, nil)
+	k := make([]int, g.n)
+	for i := 0; i < g.n; i++ {
+		for j := 0; j < g.n; j++ {
+			k[i] += g.A[i][j]
+		}
+	}
+	fmt.Println(k)
+
+	for i := 0; i < g.n; i++ {
+		for j := i; j < g.n; j++ {
+			B.SetSym(i, j, float64(g.A[i][j])-float64(k[i]*k[j])/float64(2*g.m))
+		}
+	}
+
+	// Eigen decomposition
+	e.Factorize(B, true)
+	M.EigenvectorsSym(&e)
+	v := M.ColView(g.n - 1) // Eigenvector corresponding to the largest eigenval
+	fmt.Printf("%0.2v\n\n", mat64.Formatted(v))
+
+	signs := make([]int, g.n)
+	for i := 0; i < g.n; i++ {
+		if v.At(i, 0) < 0 {
+			signs[i] = -1
+		} else {
+			signs[i] = 1
+		}
+	}
+	fmt.Println(signs)
 }
 
 func main() {
 	logging.SetBackend(BackendFormatter)
-	A := ParseGraph("karate/out.ucidata-zachary")
-	fmt.Println(A)
+	g := ParseGraph("karate/out.ucidata-zachary")
+	fmt.Println(g)
+	NewmanPartition(g)
 }
