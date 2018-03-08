fa490069991b0961c78d3f70feb4c2afd290006a
Haibao Tang <htang@humanlongevity.com>
2018-03-08 13:55:39 +0800

    [distribution] Parse outer distance file

--- distribution.go
+++ distribution.go
@@ -9,7 +9,13 @@
 
 package allhic
 
-import "fmt"
+import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
 
 // Distribution processes the distribution step
 type Distribution struct {
@@ -18,5 +24,21 @@ type Distribution struct {
 
 // Run calls the distribution steps
 func (r *Distribution) Run() {
-	fmt.Println(r.Bamfile)
+	file, _ := os.Open(r.Bamfile)
+	log.Noticef("Parse dist file `%s`", r.Bamfile)
+	reader := bufio.NewReader(file)
+	var links []int
+	for {
+		row, err := reader.ReadString('\n')
+		row = strings.TrimSpace(row)
+		if row == "" && err == io.EOF {
+			break
+		}
+		words := strings.Split(row, "\t")
+		for _, link := range strings.Split(words[1], ",") {
+			ll, _ := strconv.Atoi(link)
+			links = append(links, ll)
+		}
+	}
+	log.Noticef("Imported %d intra-contig links", len(links))
 }
